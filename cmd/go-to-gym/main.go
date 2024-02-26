07f1f431cb7680d3bfa945f779dcd2efa969387e
Daniyal Tuzelbayev <tuzelbaeff@gmail.com>
2024-02-26 22:25:57 +0600

    api server

--- cmd/go-to-gym/main.go
+++ cmd/go-to-gym/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const PORT = ":8000"
 
 func main() {
-	fmt.Println("hello")
+	log.Println("starting API server")
+	router := mux.NewRouter()
+	http.Handle("/", router)
+
+	log.Println("Server started on port", PORT)
+	err := http.ListenAndServe(PORT, router)
+	log.Fatal(err)
 }
